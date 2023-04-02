7e8dbd47fec29b187a47741318fd2627bd43c91e
edwinhati <edwinhati@outlook.com>
2023-04-02 19:00:38 +0800

    Update on env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,31 +3,29 @@ import { initializeApp } from "firebase/app";
 import {
   getAuth,
   GoogleAuthProvider,
-  setPersistence,
   signInWithPopup,
 } from "firebase/auth";
-import { useState, useEffect } from "react";
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
-//   databaseURL: process.env.NEXT_PUBLIC_FB_DATABASE_URL,
-//   projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FB_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENT_ID,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDhiEwAk6LLLr5y03R1pDztWPkPBfgzv4s",
-  authDomain: "unklabdine.firebaseapp.com",
-  databaseURL: "https://unklabdine-default-rtdb.firebaseio.com",
-  projectId: "unklabdine",
-  storageBucket: "unklabdine.appspot.com",
-  messagingSenderId: "833575028361",
-  appId: "1:833575028361:web:a300ff9e586984c79be858",
-  measurementId: "G-Q8K5ER6HHE"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDhiEwAk6LLLr5y03R1pDztWPkPBfgzv4s",
+//   authDomain: "unklabdine.firebaseapp.com",
+//   databaseURL: "https://unklabdine-default-rtdb.firebaseio.com",
+//   projectId: "unklabdine",
+//   storageBucket: "unklabdine.appspot.com",
+//   messagingSenderId: "833575028361",
+//   appId: "1:833575028361:web:a300ff9e586984c79be858",
+//   measurementId: "G-Q8K5ER6HHE"
+// };
 
 const app = initializeApp(firebaseConfig);
 

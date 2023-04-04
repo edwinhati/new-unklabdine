37b35ef3a63bc09e8f94f95d119a9a910f86e2e6
Edwin Hati <52129921+edwinhati@users.noreply.github.com>
2023-04-04 16:46:07 +0000

    implement status API

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,23 +1,53 @@
-import { createContext, ReactNode, useContext, useState, useMemo } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
 import { auth } from "@/config";
+import axios from "axios";
 
-export const UserContext = createContext({} as any);
+export const UserContext = createContext({
+  user: null,
+  loading: true,
+  responseStatus: null,
+} as { user: any; loading: boolean; responseStatus: any });
 
 export function useUser() {
   return useContext(UserContext);
 }
 
 export function UserProvider({ children }: { children: ReactNode }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<any>(null);
   const [loading, setLoading] = useState(true);
+  const [responseStatus, setResponseStatus] = useState(null);
 
-  auth.onAuthStateChanged((result: any) => {
-    setUser(result);
-    setLoading(false);
-  });
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((result: any) => {
+      setUser(result);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`/api/response/status?noreg=${user.email.split("@")[0]}`)
+        .then((res) => {
+          setResponseStatus(res.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [user]);
 
   return (
-    <UserContext.Provider value={{ user, loading }}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ user, loading, responseStatus }}>
+      {children}
+    </UserContext.Provider>
   );
 }
 
